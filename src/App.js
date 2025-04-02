dd297e4c135e18e6c387b0d3be6f7ef5cbf87686
alok <akmndljr@gmail.com>
2025-04-02 21:33:06 +0600

    aloks changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Search from './components/Search';
 import Users from './components/Users';
+import useFetch from './hook/useFetch';
+
+const url='https://jsonplaceholder.typicode.com/users';
 
 const App = () => {
   // Task 2: use custom hook
   // get data, error, isLoading states from custom hook here
   // use url: 'https://jsonplaceholder.typicode.com/users'
+  const{data,isLoading,error}=useFetch(url);
+  const[originalData,setOriginalData]=useState(data);
+  const[filteredData,setFilteredData]=useState(data);
 
   // Task 3: delete user
   // get the id from User.js
-  const handleDeleteUser = (id) => {};
+  const handleDeleteUser = (id) => {
+    const filter=data.originalData((item)=>item.id!=id)
+    setOriginalData(filter);
+    setFilteredData(filter);
+  };
 
   // Task 4: search user
   // get the text from Search.js
-  const handleSearch = (searchText) => {};
+  const handleSearch = (searchText) => {
+    const filter=originalData.filter((item)=>item.name.toLowerCase().startsWith(searchText.toLowerCase()));
+    setFilteredData(filter);
+  };
 
   return (
     <div className="container">
@@ -23,8 +36,8 @@ const App = () => {
       {error && <p>{error}</p>}
 
       {/* Needs to pass functions from here for state lifting  */}
-      <Search onHandleSearch={} />
-      {users.length > 1 && <Users users={usersCopy} onHandleDeleteUser={} />}
+      <Search onHandleSearch={handleSearch} />
+      {filteredData && <Users users={filteredData} onHandleDeleteUser={handleDeleteUser} />}
     </div>
   );
 };
--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
   // Task 1: complete this custom hook
   // step1: create 3 states: data, isLoading, error
   // step2: fetch data & handle error
   // step3: return 3 states
+  const [data,setData]=useState(null);
+  const [isLoading,setIsLoading]=useState(true);
+  const[error,setError]=useState(null);
+  useEffect(()=>{
+    fetch(url).then((res)=>{
+      if(!res.ok){
+        throw error
+      }
+      else{
+        return res.json();
+      }
+    }).then((data)=>{
+      setData(data);
+      setIsLoading(false);
+      setError(null);
+    }).catch((error)=>{
+      setError(error.message);
+      setIsLoading(false);
+    })
+  },[url]);
+  return {data,isLoading,error};
 };
 
 export default useFetch;
